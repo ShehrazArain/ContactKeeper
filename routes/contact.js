93affc666b9a67ed3c14f3398cb68b67bd526bd1
Sheraz10-hub <sarain0300@gmail.com>
2020-06-27 22:58:25 -0700

    User, Contact, Jwt API Completed

--- routes/contact.js
+++ routes/contact.js
@@ -9,7 +9,7 @@ const Contact = require('../models/Contact')
 // @route   GET api/contacts
 // @desc    Get all users contact
 // @access  Private
-router.get('/', [auth], async (req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
         const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
         res.json(contacts)
@@ -54,16 +54,52 @@ router.post('/', [auth, [
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put('/:id', (req, res) => {
-    res.send('GET all Contacts')
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if (!contact) return res.status(400).json({ msg: "Contact Not Found" })
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "Not authorized" })
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields },
+            { new: true })
+        res.send(contact)
+    } catch (err) {
+        console.error(err.message)
+        return res.status(500).json({ msg: "Server Error" })
+    }
 })
 
 
 // @route   DELETE api/contacts
 // @desc    Delete contact
 // @access  Private
-router.delete('/', (req, res) => {
-    res.send('Delete Contacts')
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if (!contact) return res.status(400).json({ msg: "Contact Not Found" })
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "Not authorized" })
+        }
+
+        await Contact.findByIdAndRemove(req.params.id);
+        res.json({ msg: "Contact removed" })
+    } catch (err) {
+        console.error(err.message)
+        return res.status(500).json({ msg: "Server Error" })
+    }
 })
 
 module.exports = router;
